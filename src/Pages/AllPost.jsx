8d676bed3997b571d23ed838e576675b1423733d
Jayesh Padwal <jayeshpadwal09@gmail.com>
2026-01-21 02:00:41 +0530

    refactor grid layout in AllPosts and Home components for improved responsiveness

--- src/Pages/AllPost.jsx
+++ src/Pages/AllPost.jsx
@@ -66,7 +66,7 @@ function AllPosts() {
                             <p className='text-[#4b4f58] dark:text-[#a7adbe]'>Loading articles...</p>
                         </div>
                     ) : userPosts.length > 0 ? (
-                        <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 sm:gap-8'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8'>
                             {userPosts.map((post, index) => (
                                 <div key={post.$id} className='h-full slide-up' style={{ animationDelay: `${index * 0.08}s` }}>
                                     <PostCard {...post} />
